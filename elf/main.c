c3db0fd9a1dadc7c05a6d363f92e68b05b2a5bd7
TalAvraham <talavrahamh@gmail.com>
2024-07-13 13:32:16 +0300

    Fixed indentations with vim

--- elf/main.c
+++ elf/main.c
@@ -5,19 +5,19 @@
 
 int main(int argc, char *argv[], char *envp[])
 {
-   FILE *f = fopen(argv[1], "rb");
+    FILE *f = fopen(argv[1], "rb");
 
-   fseek(f, 0, SEEK_END);
-   int size = ftell(f);
+    fseek(f, 0, SEEK_END);
+    int size = ftell(f);
 
-   fseek(f, 0L, SEEK_SET);
-   
-   char *buf = malloc(size);
-   fread(buf, size, 1, f);
+    fseek(f, 0L, SEEK_SET);
 
-   // Run the ELF
-   elf_run(buf, &argv[1], envp);
+    char *buf = malloc(size);
+    fread(buf, size, 1, f);
 
-   return 0;
+    // Run the ELF
+    elf_run(buf, &argv[1], envp);
+
+    return 0;
 }
 
